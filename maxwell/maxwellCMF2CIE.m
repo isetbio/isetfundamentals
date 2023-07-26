313370d2d6d2c6f69cb103868654ea3cc7f6d400
David Brainard <brainard@psych.upenn.edu>
2023-07-26 17:25:41 -0400

    Added a different way to plot.

--- maxwell/maxwellCMF2CIE.m
+++ maxwell/maxwellCMF2CIE.m
@@ -15,8 +15,8 @@ obsK = load('maxwellCMF_obsK');
 
 %% Read Observer K CMF and read XYZ corresponding wavelengths
 
-wave = obsK.wave;
-XYZ = ieReadSpectra('XYZEnergy',wave);
+waveK = obsK.wave;
+XYZK = ieReadSpectra('XYZEnergy',waveK);
 RGB = [obsK.R(:),obsK.G(:),obsK.B(:)];
 
 ieNewGraphWin;
@@ -24,8 +24,8 @@ tiledlayout(2,2);
 %%
 %{
 ieNewGraphWin;
-plot(wave,XYZ);
-plot(wave,RGB);
+plot(waveK,XYZK);
+plot(waveK,RGB);
 %}
 
 %% Find the linear transform from the CMF to the XYZ
@@ -35,30 +35,28 @@ plot(wave,RGB);
 %    pinv(RGB)*XYZ = L
 %
 %   XYZ_est = RGB*L
-L = pinv(RGB)*XYZ;
-XYZ_est = RGB*L;
+L = pinv(RGB)*XYZK;
+XYZ_estK = RGB*L;
 
 nexttile;
-plot(wave,XYZ_est); hold on;
-plot(wave,XYZ,'LineStyle','--');
+plot(waveK,XYZ_estK); hold on;
+plot(waveK,XYZK,'LineStyle','--');
 % legend('XYZ','','','Maxwell ObsK fit');
 
 %% Now for Observer J.
-
-wave = obsJ.wave;
-XYZ = ieReadSpectra('XYZEnergy',wave);
+waveJ = obsJ.wave;
+XYZJ = ieReadSpectra('XYZEnergy',waveJ);
 RGB = [obsJ.R(:),obsJ.G(:),obsJ.B(:)];
-L = pinv(RGB)*XYZ;
-XYZ_est = RGB*L;
+L = pinv(RGB)*XYZJ;
+XYZ_estJ = RGB*L;
 
 nexttile;
-plot(wave,XYZ_est); hold on;
-plot(wave,XYZ,'LineStyle','--');
+plot(waveJ,XYZ_estJ); hold on;
+plot(waveJ,XYZJ,'LineStyle','--');
 xlabel('Wavelength (nm)')
 legend('XYZ','Maxwell ObsJ fit');
 
 %%  Now the StockmanEnergy functions - observer K
-
 wave = obsK.wave;
 SS = ieReadSpectra('StockmanEnergy',wave);
 RGB = [obsK.R(:),obsK.G(:),obsK.B(:)];
@@ -90,4 +88,30 @@ plot(wave,SS,'LineStyle','--');
 xlabel('Wavelength (nm)')
 legend('SS','Maxwell ObsJ fit');
 
-%%
+% Make a slightly different figure for David
+SSK = ieReadSpectra('StockmanEnergy',waveK);
+L = pinv(SSK)*XYZK;
+XYZK_SSest = SSK*L;
+
+figure; clf; hold on;
+set(gca,'FontName','Helvetica','FontSize',26);
+plot(waveK,XYZK(:,1),'-','Color','r','LineWidth',4);
+plot(waveK,XYZK_SSest(:,1),':','Color','r','LineWidth',4);
+set(gca,'FontName','Helvetica','FontSize',14);
+plot(waveK,XYZ_estK(:,1),'o','Color','r','MarkerFaceColor','r','MarkerSize',12);
+plot(waveJ,XYZ_estJ(:,1),'s','Color','r','MarkerFaceColor','r','MarkerSize',12);
+
+plot(waveK,XYZK(:,2),'-','Color','g','LineWidth',4);
+plot(waveK,XYZK_SSest(:,2),':','Color','g','LineWidth',4);
+plot(waveK,XYZ_estK(:,2),'o','Color','g','MarkerFaceColor','g','MarkerSize',12);
+plot(waveJ,XYZ_estJ(:,2),'s','Color','g','MarkerFaceColor','g','MarkerSize',12);
+
+plot(waveK,XYZK(:,3),'-','Color','b','LineWidth',4);
+plot(waveK,XYZK_SSest(:,3),':','Color','b','LineWidth',4);
+plot(waveK,XYZ_estK(:,3),'o','Color','b','MarkerFaceColor','b','MarkerSize',12);
+plot(waveJ,XYZ_estJ(:,3),'s','Color','b','MarkerFaceColor','b','MarkerSize',12);
+
+xlabel('Wavelength (nm)','FontName','Helvetica','FontSize',22);
+ylabel('Tristimulus Value','FontName','Helvetica','FontSize',22);
+legend({'CIE 1931 XYZ', 'Stockman-Sharpe fit to XYZ', 'Maxwell 1860 Obs K fit to XYZ', 'Maxwell 1860 Obs K fit to XYZ'},'FontName','Helvetica','FontSize',18);
+title('Color Matching Over 150 Years','FontName','Helvetica','FontSize',30);
