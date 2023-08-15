82a853a3bc7ba4d25a04978f8fe897352f4c2958
Thomas Goossens <thomas.goossens@asml.com>
2023-08-15 08:04:10 +0200

    Estimating cones using new method

---
+++ wdwright/wdwEstimates_thomas.m
@@ -0,0 +1,86 @@
+%% Estimate fundamentals from WDW data
+
+wave = 400:5:650;
+
+deutanC = ieReadSpectra('wdwDeuteranopesC.mat',wave);
+protan = ieReadSpectra('wdwProtanopes.mat',wave);
+load('wdwTritanopes','obsAverage');
+tritan = interp1(obsAverage.wave,obsAverage.CMF,wave,'pchip','extrap');
+
+protan  = ieScale(protan,1);
+deutanC = ieScale(deutanC,1);
+tritan  = ieScale(tritan,1);
+
+stockman = ieReadSpectra('stockmanEnergy',wave);
+
+%%
+LdeutanC = stockman\deutanC;
+estDeutanC = stockman*LdeutanC;
+
+ieNewGraphWin;
+plot(wave,estDeutanC,'k-',wave,deutanC,'k.')
+title('Stockman to Corrected Deutan');
+xlabel('Wavelength (nm)');
+ylabel('CMF'); grid on;
+
+%% Load up the stockman conesd
+stockman = ieReadSpectra('stockmanEnergy',wave);
+ieNewGraphWin;
+plot(wave,stockman);
+grid on;
+xlabel('Wavelength (nm)');
+
+
+
+%% Plot the curves together
+
+% Everything is now within a linear transform
+ieNewGraphWin;
+plot(wave,deutanC,'g-',wave,protan,'r--',wave,tritan,'b-.');
+title('Comparing Protan Deutan and Tritan CMFs');
+xlabel('Wavelength (nm)');
+ylabel('CMF'); grid on;
+legend('deutan','','protan','','tritan','');
+
+%% Run the null space calculation
+
+ieNewGraphWin([],'tall');
+tiledlayout(3,1);
+
+% L cone
+[U,S,V] = svd([null(deutanC') ,null(tritan')]','econ');
+x = V(4,:)';
+nexttile;
+Lest = ieScale(tritan*x(1:2),1);
+Lest = ieScale(-V(:,end))
+plot(wave,Lest,'r-',wave,stockman(:,1),'r:','LineWidth',2);
+grid on;
+xlabel('Wavelength (nm)');
+title('Tritan and DeutanC')
+legend('Estimate','Stockman');
+
+% M cone
+[U,S,V] = svd([null(protan'),null(tritan')]','econ');
+x = V(4,:)';
+nexttile;
+Mest = ieScale(protan*x(1:2),1);
+Mest = ieScale(V(:,end))
+plot(wave, Mest,'g-',wave,stockman(:,2),'g:','LineWidth',2);
+grid on;
+xlabel('Wavelength (nm)');
+title('Tritan and Protan')
+legend('Estimate','Stockman');
+
+% S cone
+[U,S,V] = svd([null(deutanC'),null(protan')]','econ');
+x = V(4,:)';
+Sest = ieScale(deutanC*x(1:2),1);
+Sest = ieScale(-V(:,end))
+
+nexttile;
+plot(wave,Sest,'b-',wave,stockman(:,3),'b:','LineWidth',2);
+grid on;
+xlabel('Wavelength (nm)');
+title('DeutanC and Protan')
+legend('Estimate','Stockman');
+
