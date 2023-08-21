235d715ae63a96cded10d8feba0b3e68ddf02efb
Brian Wandell <wandell@stanford.edu>
2023-08-21 12:18:46 -0700

    Protan/Deutan data updates
    See the script wdwProtanDeutan to read the variables.  We should agree on how we store them in an easy to access format.

--- wdwright/wdwProtanDeutan.m
+++ wdwright/wdwProtanDeutan.m
@@ -1,29 +1,36 @@
 %% Wright 'spectral coefficients' for deutan and protan subjects
 %
-% These are from Fig 207 in the Color Deficient Observers book by Wright.
+% These are from 
 %
-% These are really tiny differences represented this way.  
+%    * Figures 200, 208 (CMFs)
+%    * Fig 207 (chromaticity), and
+%    * Figure 205 (Vlambda) 
 %
-% Notice that the the short-wavelength region, the values are identical.
-% This must be because all that there is down there is the S-cone.  In the
-% long-wavelength region they differ slightly.  Because M and L differ
-% slightly.
+% in the Color Deficient Observers book by Wright.
+%
+% Notes
+%
+%  There are only tiny differences between these two types of
+%  dichromats when represented w.r.t. chromaticity.
+%
+%  For example, in the short-wavelength region, the values are
+%  identical. This must be because all that there is down there is the
+%  S-cone.  In the long-wavelength region they differ slightly.
+%  Because M and L differ slightly.
 % 
-% Both are monochromatic above about 600.
+%  Both types of dichromats are essentially monochromatic above about 600.
 %
-% The scan is not flat enough to use grabit well.  I need to redo.
-%
-% I also need to get in the V lambda for these.
+%  We also the V_lambda curves for these as well (Figure 205).
 %
 
-%%  Data are stored here
+%%  Grabit data are stored here
 chdir(fullfile(iefundamentalsRootPath,'wdwright'));
 
 % We save at this resolution.  We do not have enough tritan data for all
 % this, but that's OK.
 wave = 400:700;
 
-%% Matches Figure 200
+%% Color Matching Functions: Matches Figure 200
 load('ProtanCMFBluex5.mat');
 protanBlue = interp1(ProtanCMFBluex5(:,1),ProtanCMFBluex5(:,2),wave,'pchip','extrap');
 load('ProtanCMFRed.mat');
@@ -34,8 +41,7 @@ plot(wave,protanBlue,'b-',wave,protanRed,'r-');
 grid on;
 title("Matches Figure 200")
 
-protan = [protanRed(:), protanBlue(:)];
-
+protanCMF = [protanRed(:), protanBlue(:)];
 
 %% Matches Figure 208
 load('DeutanCMFBluex5.mat');
@@ -47,7 +53,7 @@ plot(wave,deutanBlue,'b-',wave,deutanRed,'r-');
 grid on;
 title("Matches Figure 208")
 
-deutan = [deutanRed(:), deutanBlue(:)];
+deutanCMF = [deutanRed(:), deutanBlue(:)];
 
 %% Now compare with Stockman
 
@@ -58,25 +64,27 @@ ieNewGraphWin([],'wide');
 tiledlayout(1,2);
 
 % protan = stockman*L
-Lprotan = stockman\protan;
+Lprotan = stockman\protanCMF;
 estProtan = stockman*Lprotan;
 nexttile;
-plot(wave,estProtan,'k-',wave,protan,'k.')
+plot(wave,estProtan,'k-',wave,protanCMF,'k.')
 title('Stockman to Protan (Fig 200)');
 xlabel('Wavelength (nm)');
 ylabel('CMF'); grid on;
 
 nexttile;
-Ldeutan = stockman\deutan;
+Ldeutan = stockman\deutanCMF;
 estDeutan = stockman*Ldeutan;
-plot(wave,estDeutan,'k-',wave,deutan,'k.')
+plot(wave,estDeutan,'k-',wave,deutanCMF,'k.')
 title('Stockman to Deutan (Fig 208)');
 xlabel('Wavelength (nm)');
 ylabel('CMF'); grid on;
 
 %% Does not normally run.
 
-% The blue curve in the book seems off. 
+% The blue curve in the book seems off. We copy the protan blue curve
+% into deutan because that one made sense.  We call the corrected
+% deutan with the variable deutanC.
 deutanC = deutan;
 deutanC(:,2) = protan(:,2);
 
@@ -87,7 +95,7 @@ if saveFlag
     ieSaveSpectralFile(wave,protan,'WDW data from book figures 198-208','wdwProtanopes.mat');    
 end
 
-%% Check that we got
+% Check that we wrote correctly
 if saveFlag
     DC = ieReadSpectra('wdwDeuteranopesC.mat',wave);
     D = ieReadSpectra('wdwDeuteranopes.mat',wave);
@@ -97,4 +105,38 @@ if saveFlag
     plot(wave,DC,'--',wave,D,':',wave,P,'-.');
 end
 
-%%
+%% Chromaticities (Figure 207)
+
+load('wdwChromRedProtan.mat');
+protanChromRed = interp1(wdwProtanChromRed(:,1),wdwProtanChromRed(:,2),wave,'pchip','extrap');
+load('wdwChromRedDeutan.mat');
+deutanChromRed = interp1(wdwDeutanChromRed(:,1),wdwDeutanChromRed(:,2),wave,'pchip','extrap');
+
+% Wright calls these spectral coefficient curves
+protanChromaticity = [protanChromRed(:),1 - protanChromRed(:)];
+deutanChromaticity = [deutanChromRed(:),1 - deutanChromRed(:)];
+
+ieNewGraphWin;
+plot(wave,protanChromaticity,'r-',wave,deutanChromaticity,'g--','Linewidth',2);
+grid on;
+xlabel('Wavelength (nm)'); ylabel('Chromaticity');
+legend('Protan','','Deutan','');
+title('Spectral coefficients for primaries: 460, 650');
+
+%% Vlambda (Figure 205)
+
+load('wdwVlambda_Protan.mat');
+vlambdaProtan = interp1(Wright_Protan_Vlambda(:,1),Wright_Protan_Vlambda(:,2),wave,'pchip','extrap');
+load('wdwVlambda_Deutan.mat');
+vlambdaDeutan = interp1(Wright_Deutan_Vlambda(:,1),Wright_Deutan_Vlambda(:,2),wave,'pchip','extrap');
+load('wdwVlambda_Trichromat.mat');
+vlambdaTrichromat = interp1(Wright_Trichromat_Vlambda(:,1),Wright_Trichromat_Vlambda(:,2),wave,'pchip','extrap');
+
+ieNewGraphWin;
+plot(wave,vlambdaTrichromat,'k-',wave,vlambdaDeutan,'g-',wave,vlambdaProtan,'r-');
+grid on;
+xlabel('Wavelength (nm)'); ylabel('Chromaticity');
+legend('Trichromat','Deutan','Protan');
+title('Vlambda');
+
+%% End
