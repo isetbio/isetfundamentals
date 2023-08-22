5908a2378801e209680c94cddbcf6e310320bacb
David Brainard <brainard@psych.upenn.edu>
2023-08-22 17:20:48 -0400

    Fit with Stockman-Sharpe.  Deutan B is still not good.

--- wdwright/wrightProtanDeutanCMFsFromChromLum.m
+++ wdwright/wrightProtanDeutanCMFsFromChromLum.m
@@ -93,7 +93,7 @@ RPrimaryWlIndex = find(wave == RPrimaryWl);
 BPrimaryWlIndex = find(wave == BPrimaryWl);
 wdwNormWl = 494;
 
-% Find R and B from rWDW, and Vlambda
+%% Find R and B from rWDW, and Vlambda
 %
 % The search method was in intermediate step.  I will
 % delete once my understanding of all this is a little
@@ -123,7 +123,7 @@ theB_Deutan_Data = load(fullfile(iefundamentalsRootPath,'wdwright','DeutanCMFBlu
 B_Deutan_Tabulated = interp1(theB_Deutan_Data.DeutanCMFBluex5(:,1),theB_Deutan_Data.DeutanCMFBluex5(:,2),wave,'spline',NaN)/5;
 VlambdaFromRplusB_Deutan_Tabulated = R_Deutan_Tabulated + B_Deutan_Tabulated;
 
-% Get Vlambda as sum or R + B and add to plot as a check
+%% Get Vlambda as sum of R + B and add to plot as a check
 figure(lumFig);
 subplot(1,3,1);
 plot(wave,VlambdaFromRplusB_Protan_Derived,'k-','LineWidth',2);
@@ -138,20 +138,40 @@ ylim([0 1.2]);
 xlabel('Wavelength (nm)');
 ylabel('Luminance');
 
-% Plot derived CMFs
+%% Load Stockman-Sharpe cones and fit CMFs with them
+theCones = load('T_cones_ss2.mat');
+T_cones = SplineCmf(theCones.S_cones_ss2,theCones.T_cones_ss2,wave);
+
+index = ~isnan(R_Protan_Derived);
+waveFitProtan = wave(index);
+R_Protan_Derived_Fit = (T_cones(:,index)'*(T_cones(:,index)'\R_Protan_Derived(index)))';
+B_Protan_Derived_Fit = (T_cones(:,index)'*(T_cones(:,index)'\B_Protan_Derived(index)))';
+
+index = ~isnan(R_Deutan_Derived);
+waveFitDeutan = wave(index);
+R_Deutan_Derived_Fit = (T_cones(:,index)'*(T_cones(:,index)'\R_Deutan_Derived(index)))';
+B_Deutan_Derived_Fit = (T_cones(:,index)'*(T_cones(:,index)'\B_Deutan_Derived(index)))';
+
+%% Plot derived CMFs
 cmfFig = figure;
-subplot(1,2,1); hold on;
-plot(wave,R_Protan_Derived,'r-','LineWidth',3);
+set(gcf,'Position',[100 100 1200 600]);
+
+subplot(1,3,1); hold on;
+plot(wave,R_Protan_Derived,'r-','LineWidth',5);
+plot(waveFitProtan,R_Protan_Derived_Fit,'y-','LineWidth',3);
 plot(wave,R_Protan_Tabulated,'k-','LineWidth',1);
-plot(wave,5*B_Protan_Derived,'b-','LineWidth',3);
+plot(wave,5*B_Protan_Derived,'b-','LineWidth',5);
+plot(waveFitProtan,5*B_Protan_Derived_Fit,'y-','LineWidth',3);
 plot(wave,5*B_Protan_Tabulated,'k-','LineWidth',1);
 xlabel('Wavelength (nm)');
 ylabel('CMF');
 title('Protan CMFs');
-subplot(1,2,2); hold on;
-plot(wave,R_Deutan_Derived,'r-','LineWidth',3);
+subplot(1,3,2); hold on;
+plot(wave,R_Deutan_Derived,'r-','LineWidth',5);
+plot(waveFitDeutan,R_Deutan_Derived_Fit,'y-','LineWidth',3);
 plot(wave,R_Deutan_Tabulated,'k-','LineWidth',1);
-plot(wave,5*B_Deutan_Derived,'b-','LineWidth',3);
+plot(wave,5*B_Deutan_Derived,'b-','LineWidth',5);
+plot(waveFitDeutan,5*B_Deutan_Derived_Fit,'y-','LineWidth',3);
 plot(wave,5*B_Deutan_Tabulated,'k-','LineWidth',1);
 xlabel('Wavelength (nm)');
 ylabel('CMF');
@@ -162,171 +182,27 @@ save(fullfile(iefundamentalsRootPath,'wdwright','Wright_ProtanDeutanDerivedCMFs'
     'R_Protan_Derived','B_Protan_Derived','R_Deutan_Derived','B_Deutan_Derived', ...
     'R_Protan_Tabulated','B_Protan_Tabulated','R_Deutan_Tabulated','B_Deutan_Tabulated');
 
-    % % Generate WDW scaled CMFs for computation of check r_WDW and g_WDW.
-    % RAtNormWl_Derived = interp1(wave,R_Derived,wdwNormWl,interpMethod);
-    % GAtNormWl_Derived = interp1(wave,B_Derived,wdwNormWl,interpMethod);
-    % fprintf('Derived R and G at %0.2f nm; R = %0.3f; G = %0.3f\n', ...
-    %     wdwNormWl,RAtNormWl_Derived,GAtNormWl_Derived);
-    % RToGAtNormWl_Derived(oo) = RAtNormWl_Derived/GAtNormWl_Derived;
-    % RWDW_Derived = R_Derived/RToGAtNormWl_Derived(oo);
-    % GWDW_Derived = B_Derived;
-    % rWDW_FromDerivedCMF  = RWDW_Derived./(RWDW_Derived + GWDW_Derived);
-    % gWDW_FromDerivedCMF  = GWDW_Derived./(RWDW_Derived + GWDW_Derived);
+    
 
-    % % Scale the two back to be as close to the tabulated values
-    % % as possible
-    % CMFScalarDerivedToTabulated(oo) = [R_Derived ; B_Derived]\[RG_Tabulated(:,1) ; RG_Tabulated(:,2)];
-    % R_DerivedScaledToTabulated = R_Derived*CMFScalarDerivedToTabulated(oo);
-    % G_DerivedScaledToTabulated = B_Derived*CMFScalarDerivedToTabulated(oo);
-    % 
-    % % In the end, we only care about the CMFs up to linear transformation
-    % % Find best linear transform from derived to tabulated
-    % M_DerivedToTabulated = [R_Derived B_Derived]\[RG_Tabulated(:,1) RG_Tabulated(:,2)];
-    % RG_DerivedLinearToTabulated = [R_Derived B_Derived]*M_DerivedToTabulated;
-    % R_DerivedLinearToTabulated = RG_DerivedLinearToTabulated(:,1);
-    % G_DerivedLinearToTabulated = RG_DerivedLinearToTabulated(:,2);
-    % 
-    % % Scale factor to bring our WDW scaled derived CMFs into alignment with 
-    % % our WDW scaled tabulated CMFs.
-    % WDWCMFScalarDerivedToTabulated(oo) = [RWDW_Derived ; GWDW_Derived]\[RWDW_Tabulated ; GWDW_Tabulated];
-
-    % % Plot to check that it all works
-    % figure;
-    % set(gcf,'Position',[500 500 1600 1200]);
-    % subplot(2,4,1); hold on
-    % plot(wave,Vlambda_Tabulated,'r-','LineWidth',6);
-    % plot(wave,VlambdaFromRplusG_Tabulated,'b-','LineWidth',4);
-    % plot(wave,VlambdaFromRplusG_Tabulated,'g-','LineWidth',2);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Wavelength (nm)','FontName','Helvetica','FontSize',18);
-    % ylabel('Primary Intensity','FontName','Helvetica','FontSize',18);
-    % title([observer ' Derived and Tabulated Luminance'],'FontName','Helvetica','FontSize',14);
-    % legend({'Tabulated', 'Tabulated R + G', 'Derived R + G'},'FontName','Helvetica','FontSize',10,'Location','NorthWest');
-    % text(550,5,sprintf('Vr/Vg = %0.2f',VrOverVg_Tabulated(oo)),'FontName','Helvetica','FontSize',10);
-    % grid on;
-    % 
-    % subplot(2,4,5); hold on
-    % maxVal = max([Vlambda_Tabulated(:) ; VlambdaFromRplusG_Tabulated(:)]);
-    % plot(Vlambda_Tabulated,VlambdaFromRplusG_Tabulated,'ro','MarkerFaceColor','b','MarkerSize',12);
-    % plot(Vlambda_Tabulated,VlambdaFromRplusB_Derived,'bo','MarkerFaceColor','g','MarkerSize',8);
-    % plot([0 maxVal],[0 maxVal],'k-','LineWidth',1);
-    % xlim([0 maxVal]); ylim([0 maxVal]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Tabulated vLambda','FontName','Helvetica','FontSize',18);
-    % ylabel('Derived vLambda','FontName','Helvetica','FontSize',18);
-    % title([observer ' Derived vs Tabulated Luminance'],'FontName','Helvetica','FontSize',14);
-    % axis('square');
-    % legend({'Tabulated R + G','Derived R + G'},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,2); hold on;
-    % plot(wave,RG_Tabulated(:,1),'r-','LineWidth',6);
-    % plot(wave,R_DerivedScaledToTabulated,'b-','LineWidth',4);
-    % plot(wave,R_DerivedLinearToTabulated,'g-','LineWidth',2);
-    % plot(wave,RG_Tabulated(:,2),'r-','LineWidth',6);
-    % plot(wave,G_DerivedScaledToTabulated,'b-','LineWidth',4);
-    % plot(wave,G_DerivedLinearToTabulated,'g-','LineWidth',2);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Wavelength (nm)','FontName','Helvetica','FontSize',18);
-    % ylabel('CMF','FontName','Helvetica','FontSize',18);
-    % title([observer ' Tabulated and Derived CMFs'],'FontName','Helvetica','FontSize',14);
-    % legend({'Tabulated', 'Derived, Scaled', 'Derived, LinTran'},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,6); hold on
-    % maxVal = max([RG_Tabulated(:,1); RG_Tabulated(:,2); R_Derived(:) ; B_Derived(:)]);
-    % plot(RG_Tabulated(:,1),R_DerivedScaledToTabulated,'bo','MarkerFaceColor','b','MarkerSize',12);
-    % plot(RG_Tabulated(:,1),R_DerivedLinearToTabulated,'go','MarkerFaceColor','g','MarkerSize',8);
-    % plot(RG_Tabulated(:,2),G_DerivedScaledToTabulated,'bo','MarkerFaceColor','b','MarkerSize',12);
-    % plot(RG_Tabulated(:,2),G_DerivedLinearToTabulated,'go','MarkerFaceColor','g','MarkerSize',8);
-    % plot([0 maxVal],[0 maxVal],'k-','LineWidth',1);
-    % xlim([0 maxVal]); ylim([0 maxVal]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Tabulated CMF','FontName','Helvetica','FontSize',18);
-    % ylabel('Derived CMF','FontName','Helvetica','FontSize',18);
-    % axis('square');
-    % title([observer ' Derived vs Tabulated CMFs'],'FontName','Helvetica','FontSize',14);
-    % legend({'Derived, Scaled', 'Derived, LinTran'},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,3); hold on;
-    % plot(wave,rgWDW_Tabulated(:,1),'r-','LineWidth',6);
-    % plot(wave,rWDW_FromTabulatedCMF,'b-','LineWidth',4);
-    % plot(wave,rWDW_FromDerivedCMF,'g-','LineWidth',2);
-    % plot(wave,rgWDW_Tabulated(:,2),'r-','LineWidth',6);
-    % plot(wave,gWDW_FromTabulatedCMF,'b-','LineWidth',4);
-    % plot(wave,gWDW_FromDerivedCMF,'g-','LineWidth',2);
-    % plot([wdwNormWl wdwNormWl],[0 1]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Wavelength (nm)','FontName','Helvetica','FontSize',18);
-    % ylabel(['WDW rg'],'FontName','Helvetica','FontSize',18);
-    % ylim([-0.3 1.3]);
-    % title([observer ' WDW rg'],'FontName','Helvetica','FontSize',14);
-    % legend({'Tabulated', 'From Tabulated CMF', 'From Derived CMF'},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,7); hold on
-    % minVal = -0.3;
-    % maxVal = 1.3;
-    % plot(rgWDW_Tabulated(:,1),rWDW_FromTabulatedCMF,'bo','MarkerFaceColor','b','MarkerSize',12);
-    % plot(rgWDW_Tabulated(:,1),rWDW_FromDerivedCMF,'go','MarkerFaceColor','g','MarkerSize',8);
-    % % plot(rgWDW_Tabulated(:,2),gWDW_FromTabulatedCMF,'bo','MarkerFaceColor','b','MarkerSize',12);
-    % % plot(rgWDW_Tabulated(:,2),gWDW_FromDerivedCMF,'go','MarkerFaceColor','g','MarkerSize',8);
-    % plot([minVal maxVal],[minVal maxVal],'k-','LineWidth',1);
-    % xlim([minVal maxVal]); ylim([minVal maxVal]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Tabulated WDW rg','FontName','Helvetica','FontSize',18);
-    % ylabel('Derived WDW rg','FontName','Helvetica','FontSize',18);
-    % axis('square');
-    % title([observer ' WDW rg'],'FontName','Helvetica','FontSize',14);
-    % legend({'From Tabulated CMFs', 'From Derived CMF',},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,4); hold on;
-    % maxVal = max([RWDW_Tabulated ; RWDW_Derived ; GWDW_Tabulated ; GWDW_Derived ]);
-    % plot(wave,RWDW_Tabulated,'r-','LineWidth',6);
-    % plot(wave,WDWCMFScalarDerivedToTabulated(oo)*RWDW_Derived,'b-','LineWidth',4);
-    % plot(wave,RWDW_Derived,'g-','LineWidth',1);
-    % plot(wave,GWDW_Tabulated,'r-','LineWidth',6);
-    % plot(wave,WDWCMFScalarDerivedToTabulated(oo)*GWDW_Derived,'b-','LineWidth',4);
-    % plot(wave,GWDW_Derived,'g-','LineWidth',1);
-    % plot([wdwNormWl wdwNormWl],[0 maxVal]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Wavelength (nm)','FontName','Helvetica','FontSize',18);
-    % ylabel(['WDW RG'],'FontName','Helvetica','FontSize',18);
-    % title([observer ' WDW RG'],'FontName','Helvetica','FontSize',14);
-    % legend({'Tabulated', 'Derived, Scaled', 'Derived'},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-    % 
-    % subplot(2,4,8); hold on
-    % minVal = -0.3;
-    % maxVal = 1.3;
-    % x = RWDW_Tabulated./(RWDW_Tabulated+GWDW_Tabulated);
-    % y = RWDW_Derived./(RWDW_Derived+GWDW_Derived);
-    % plot(x,y,'bo','MarkerFaceColor','b','MarkerSize',12);
-    % plot([minVal maxVal],[minVal maxVal],'k-','LineWidth',1);
-    % xlim([minVal maxVal]); ylim([minVal maxVal]);
-    % set(gca,'FontName','Helvetica','FontSize',16);
-    % xlabel('Tabulated WDW r','FontName','Helvetica','FontSize',18);
-    % ylabel('Derived WDW r','FontName','Helvetica','FontSize',18);
-    % axis('square');
-    % title([observer ' WDW rg'],'FontName','Helvetica','FontSize',14);
-    % %legend({'From Tabulated CMFs', 'From Derived CMF',},'FontName','Helvetica','FontSize',8,'Location','NorthWest');
-    % grid on;
-
-    % Save figure
-    %saveas(gcf,fullfile(iefundamentalsRootPath,'wdwright',['Wright_Tritan_' observer '_Check']),'tiff');
-
-function [R,G] = WDWChromVlambdaToCMFFun(W1,wls,rWDW,Vlambda)
-% Given W1, find R and G from rWDW and Vlambda
+function [R,B] = WDWChromVlambdaToCMFFun(W1,wls,rWDW,Vlambda)
+% Given W1, find R and B from rWDW and Vlambda.
+%
+% Input W1 is the scalar used to normalize R into line with B for the WDW
+% coordinates, via RWDW = R/W1.
+%
+% Inputs wls, rWDW and Vlambda should be column vectors, with the latter
+% two providing the values at the wavelengths in the former.
 %
 % We know:
 %   R_WDW = (R/W1)
-%   G_WDW = G
-%   R + G = Vlamda
-%   r_WDW = R_WDW/(R_WDW + G_WDW) = (R/W1)/(R/W1 + G) = (R/W1)/(R/W1 + Vlambda - R)
+%   B_WDW = B
+%   R + B = Vlamda
+%   r_WDW = R_WDW/(R_WDW + B_WDW) = (R/W1)/(R/W1 + B) = (R/W1)/(R/W1 + Vlambda - R)
 %
 % At each wavelength, solve for R given r_WDW, Vlamda, and W1. 
+%
+% If either rWDW or Vlambda is NaN at a wavelength, NaN is returned for
+% that wavelength.
 
 % Use fsolve to find R and G at each wavelength, given W1, r_WDW and
 % Vlambda at each wavelength.close all
@@ -339,6 +215,6 @@ for ww = 1:length(wls)
     end
 end
 R = R';
-G = Vlambda - R;
+B = Vlambda - R;
 
 end
