b9b0439923ebd28dd661d45130af2fd10ca555ef
Brian Wandell <wandell@stanford.edu>
2023-08-29 10:21:36 -0700

    plotting formatting

--- wdwright/wdwStockman.m
+++ wdwright/wdwStockman.m
@@ -16,10 +16,6 @@
 
 %% Protanope CMF
 
-ieNewGraphWin([],'big');
-pColor = [0.7 0.7 0.7];
-tiledlayout(2,2);
-
 % Protan
 fname = fullfile(iefundamentalsRootPath,'wdwright','cmfProtan.mat');
 load(fname,'wave','cmfProtan');
@@ -29,29 +25,36 @@ stockman = ieReadSpectra('stockmanEnergy',wave);
 % Fig 200
 Lprotan = stockman\cmfProtan;
 estProtan = stockman*Lprotan;
-nexttile;
-plot(wave,estProtan,'k-','Linewidth',3);
-hold on; plot(wave,cmfProtan,'.','Color',pColor,'Linewidth',1);
-title('Protan');
-xlabel('Wavelength (nm)');
-ylabel('Primary intensity (a.u.)'); grid on;
 
 % Deutan
 fname = fullfile(iefundamentalsRootPath,'wdwright','cmfDeutan.mat');
 load(fname,'wave','cmfDeutan');
-stockman = ieReadSpectra('stockmanEnergy',wave);
+
+% The wave is the same.  So no need to reread.
+% stockman = ieReadSpectra('stockmanEnergy',wave);
+
+ieNewGraphWin([],'big');
+pColor = [1 1 1]*0;
+tiledlayout(2,2);
+
+nexttile;
+plot(wave(1:2:end),estProtan(1:2:end,:),'ko','Linewidth',1,'MarkerSize',2);
+hold on; plot(wave,cmfProtan,'-','Color',pColor,'Linewidth',2);
+title('Protan');
+xlabel('Wavelength (nm)');
+ylabel('Primary intensity (a.u.)'); grid on;
 
 nexttile;
 Ldeutan = stockman\cmfDeutan;
 estDeutan = stockman*Ldeutan;
-plot(wave,estDeutan,'k-','Linewidth',3);
+plot(wave(1:2:end),estDeutan(1:2:end,:),'ko','Linewidth',1,'MarkerSize',2);
 hold on;
-plot(wave,cmfDeutan,'.','Color',pColor,'Linewidth',1)
+plot(wave,cmfDeutan,'-','Color',pColor,'Linewidth',2)
 title('Deutan (Original)');
 xlabel('Wavelength (nm)');
 ylabel('Primary intensity (a.u.)'); grid on;
 
-% Tritan
+% Tritan - the wavelength is different
 fname = fullfile(iefundamentalsRootPath,'wdwright','cmfTritan.mat');
 load(fname,'obsAverage');
 % cmfTritan = obsAverage.CMF;
@@ -62,13 +65,13 @@ stockman = ieReadSpectra('stockmanEnergy',wave);
 nexttile;
 Ltritan = stockman\cmfTritan;
 estTritan = stockman*Ltritan;
-plot(wave,estTritan,'k-', 'LineWidth',3);
-hold on; plot(wave,cmfTritan,'.','Color',pColor,'Linewidth',1)
+plot(wave(1:2:end),estTritan(1:2:end,:),'ko', 'LineWidth',1,'MarkerSize',2);
+hold on; plot(wave,cmfTritan,'-','Color',pColor,'Linewidth',2)
 title('Tritan');
 xlabel('Wavelength (nm)');
 ylabel('Primary intensity (a.u.)'); grid on;
 
-% DeutanC
+% DeutanC - back to original wave
 fname = fullfile(iefundamentalsRootPath,'wdwright','cmfDeutanC.mat');
 load(fname,'wave','cmfDeutanC');
 stockman = ieReadSpectra('stockmanEnergy',wave);
@@ -76,9 +79,9 @@ stockman = ieReadSpectra('stockmanEnergy',wave);
 nexttile;
 LdeutanC = stockman\cmfDeutanC;
 estDeutanC = stockman*LdeutanC;
-plot(wave,estDeutanC,'k-','Linewidth',3);
+plot(wave(1:2:end),estDeutanC(1:2:end,:),'ko','Linewidth',1,'MarkerSize',2);
 hold on;
-plot(wave,cmfDeutanC,'.','Color',pColor,'Linewidth',1)
+plot(wave,cmfDeutanC,'-','Color',pColor,'Linewidth',2)
 title('Deutan (Corrected)');
 xlabel('Wavelength (nm)');
 ylabel('Primary intensity (a.u.)'); grid on;
