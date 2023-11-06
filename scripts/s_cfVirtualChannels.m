0dbd563be863cf366d4d49224c42e2c76403b8c3
Brian Wandell <wandell@stanford.edu>
2023-11-05 16:23:34 -0800

    Update s_cfVirtualChannels.m

--- scripts/s_cfVirtualChannels.m
+++ scripts/s_cfVirtualChannels.m
@@ -41,18 +41,20 @@ for ii=1:2
 end
 
 % Device B
-B = [0 0; 1 0; 0 1];
+B = [0 0; 1 .3; .2 1];
 for ii=1:2
     B(:,ii) = B(:,ii)/vectorLength(B(:,ii));
 end
 
-%%
+%% Graph many virtual channels for A
+
+ieNewGraphWin;
 
 % Define the origin (O)
 O = [0, 0, 0];
-wgts = 
-Aspace = A*[]
-P = A';
+[x,y] = ieCirclePoints;
+Aspace = A*[x(:),y(:)]';
+P = Aspace';
 
 
 % Extract x, y, and z coordinates of P
@@ -61,14 +63,53 @@ Y = P(:, 2);
 Z = P(:, 3);
 
 % Create a quiver plot
-ieNewGraphWin;
+for ii=1:numel(X)
+    hold on;
+    quiver3(O(1), O(2), O(3), X(ii), Y(ii), Z(ii),'off','b');
+end
+grid on;
+
+
+%% Add the channels for the first device
+P = A';
+X = P(:, 1);
+Y = P(:, 2);
+Z = P(:, 3);
+
 for ii=1:2
     hold on;
-    quiver3(O(1), O(2), O(3), X(ii), Y(ii), Z(ii),0);
+    L = quiver3(O(1), O(2), O(3), X(ii), Y(ii), Z(ii),'off','b');
+    L.LineWidth = 2; 
 end
 
-% Customize the plot (optional)
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
-title('3D Arrow Plot');
+%% Add the channels for the second device
+P = B';
+X = P(:, 1);
+Y = P(:, 2);
+Z = P(:, 3);
+
+for ii=1:2
+    hold on;
+    L = quiver3(O(1), O(2), O(3), X(ii), Y(ii), Z(ii),'off','r');
+    L.LineWidth = 2;
+end
+ 
+set(gca,'zlim',[-1 1])
+view(145,22);
+[az el] = view
+%% Show more of the B space possibilities, including the intersection
+
+Bspace = B*[x(:),y(:)]';
+P = Bspace';
+
+% Extract x, y, and z coordinates of P
+X = P(:, 1);
+Y = P(:, 2);
+Z = P(:, 3);
+
+% Create a quiver plot
+for ii=1:numel(X)
+    hold on;
+    quiver3(O(1), O(2), O(3), X(ii), Y(ii), Z(ii),'off','r');
+end
+grid on;
