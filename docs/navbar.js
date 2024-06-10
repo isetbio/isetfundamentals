b05722ecff2d02cc0f66a2bb09b70e161aa1228e
Brocoliman <lucaswang616@gmail.com>
2024-06-10 16:56:59 -0700

    Navbar Fix #3

--- docs/navbar.js
+++ docs/navbar.js
@@ -3,10 +3,12 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('navbar.html')
         .then(response => response.text())
         .then(data => {
+            // Extract the <head> content and insert it into the document head
             const headContent = data.match(/<head[^>]*>([\s\S]*?)<\/head>/i);
             if (headContent) {
                 document.head.innerHTML += headContent[1];
             }
+            // Remove the head part from data and insert the rest into the navbar div
             const bodyContent = data.replace(/<head[^>]*>([\s\S]*?)<\/head>/i, '');
             document.getElementById('navbar').innerHTML = bodyContent;
         })
